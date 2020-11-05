f7f83f4d4de637561b43bb7b0364bcc16135d9e8
Pradeep Mahato <pradeepmahato007@gmail.com>
2020-11-04 22:05:34 -0800

    Redis util added

--- include/commonHeader.h
+++ include/commonHeader.h
@@ -18,9 +18,16 @@
 #define DISPLAY_MAP 0
 #endif
 
+#ifndef SERVER_MODE
+#define SERVER_MODE 0
+#endif
+
 #include <bits/stdc++.h>
 #include <fstream>
 #include <omp.h>
+#include <sw/redis++/redis++.h>
+#include <thread>
+#include <chrono>
 
 #define maxOf(a, b) ((a) > (b) ? (a) : (b))
 #define minOf(a, b) ((a) < (b) ? (a) : (b))
@@ -29,4 +36,23 @@ typedef long long unsigned lln;
 typedef long double lld;
 
 using namespace std;
+using namespace sw::redis;
+
+
+class RequestObject {
+    public:
+    string requestId, pattern, event_length, status, input_file_path, output_file_path;
+    
+    RequestObject() {
+    }
+    
+    RequestObject(string requestId, string pattern, string event_length, string status, string input_file_path, string output_file_path) {
+        this->requestId = requestId;
+        this->pattern = pattern;
+        this->event_length = event_length;
+        this->status = status;
+        this->input_file_path = input_file_path;
+        this->output_file_path = output_file_path;
+    }
+};
 #endif
